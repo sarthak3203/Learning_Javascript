939016f29592b8c421ddb91368928ec02c6f8be8
Sarthak Sharma <sarthak3203@gmail.com>
2025-03-21 19:47:11 +0530

    completed myntra with javascript

--- Projects/Myntra/scripts/index.js
+++ Projects/Myntra/scripts/index.js
@@ -1,19 +1,52 @@
-let itemsContainerElement = document.querySelector('.items-container');
-itemsContainerElement.innerHTML=`  <div class="item-container">
-                <img class="item-image" src="images/1.jpg" alt="item image">
-                <div class="rating">
-                    4.5⭐ | 1.4k
-                </div>
-                <div class="company-name">
-                    Carlton London
-                </div>
-                <div class="item-name">
-                    Rhodium Plated CZ floral
-                </div>
-                <div class="price">
-                    <span class="current-price">666</span>
-                    <span class="original-price">1099</span>
-                    <span class="discount">42% off</span>
-                </div>
-                <button class="add-to-bag-btn">Add To Bag</button>
-            </div>`;
+let bagItems;
+onLoad();
+
+function onLoad() {
+  let bagItemsStr = localStorage.getItem('bagItems');
+  bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];
+  displayItemsOnHomePage();
+  displayBagIcon();
+}
+
+function addToBag(itemId) {
+  bagItems.push(itemId);
+  localStorage.setItem('bagItems', JSON.stringify(bagItems));
+  displayBagIcon();
+}
+
+function displayBagIcon() {
+  let bagItemCountElement = document.querySelector('.bag-item-count');
+  if (bagItems.length > 0) {
+    console.log('I am here');
+    bagItemCountElement.style.visibility = 'visible';
+    bagItemCountElement.innerText = bagItems.length;
+  } else {
+    bagItemCountElement.style.visibility = 'hidden';
+  }
+}
+
+function displayItemsOnHomePage() {
+  let itemsContainerElement = document.querySelector('.items-container');
+  if (!itemsContainerElement) {
+    return;
+  }
+  let innerHtml = '';
+  items.forEach(item => {
+    innerHtml += `
+    <div class="item-container">
+      <img class="item-image" src="${item.image}" alt="item image">
+      <div class="rating">
+          ${item.rating.stars} ⭐ | ${item.rating.count}
+      </div>
+      <div class="company-name">${item.company}</div>
+      <div class="item-name">${item.item_name}</div>
+      <div class="price">
+          <span class="current-price">Rs ${item.current_price}</span>
+          <span class="original-price">Rs ${item.original_price}</span>
+          <span class="discount">(${item.discount_percentage}% OFF)</span>
+      </div>
+      <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
+    </div>`
+  });
+  itemsContainerElement.innerHTML = innerHtml;
+}
